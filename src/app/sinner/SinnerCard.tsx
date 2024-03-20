301a009f2d49b60d74e3c8645f1f3f3061952da9
Zetab_S <songkm2@naver.com>
2024-03-20 11:36:50 +0900

    wip: 수감자 카드

---
+++ src/app/sinner/SinnerCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SinnerCard: React.FC = () => {
+  return <div className="flex h-full w-full items-center justify-center"></div>;
+};
+
+export default SinnerCard;
