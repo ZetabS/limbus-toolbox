6b03451bc918472277cd4836f0e89fa71d310e4b
Zetab_S <songkm2@naver.com>
2024-03-20 11:33:33 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
+import React from 'react';
 import Image from 'next/image';
 
-export default function Home() {
-  const lis = [];
-  for (let i = 0; i < 100; i++) {
-    lis.push(
-      <div>
-        asdf
-        <br />
-      </div>
-    );
-  }
-  return lis;
-}
+const Home: React.FC = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="">Home</div>
+    </div>
+  );
+};
+export default Home;
