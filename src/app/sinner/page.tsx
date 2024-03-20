6c3d7c390e3d768518fa8fe2bf47e503e3177482
Zetab_S <songkm2@naver.com>
2024-03-20 11:36:40 +0900

    feat: 수감자 필터 컴포넌트

--- src/app/sinner/page.tsx
+++ src/app/sinner/page.tsx
@@ -1,3 +1,31 @@
-export default function limbusFilter() {
-  return <>수감자 목록</>;
+'use client';
+import Filter from '@/app/sinner/Filter';
+import React from 'react';
+
+interface FilterValue {
+  name: string;
+  initialFilters: string[];
 }
+
+const Sinner: React.FC = () => {
+  const filterValues: FilterValue[] = [
+    { name: '공격 유형', initialFilters: ['참격', '관통', '타격'] }
+  ];
+
+  return (
+    <div className="flex h-max w-full flex-col items-center justify-center p-4">
+      {filterValues.map(({ name, initialFilters }) => {
+        return (
+          <Filter
+            key={name}
+            name={name}
+            initialFilters={initialFilters}
+            onSelectFilter={() => {}}
+          ></Filter>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Sinner;
