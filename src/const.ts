e516bbdc9649b32b3e7fc0c79e94e8c2a8c2907a
Zetab_S <songkm2@naver.com>
2024-03-23 16:43:04 +0900

    feat: const.ts 추가

---
+++ src/const.ts
@@ -0,0 +1,29 @@
+export const SINNER_NAME = {
+  YISANG: 'yi_sang',
+  FAUST: 'faust',
+  DONQUIXOTE: 'don_quixote',
+  RYOSHU: 'ryoshu',
+  MEURSAULT: 'meursault',
+  HONGLU: 'hong_lu',
+  HEATHCLIFF: 'heathcliff',
+  ISHMAEL: 'ishmael',
+  RODION: 'rodion',
+  SINCLAIR: 'sinclair',
+  OUTIS: 'outis',
+  GREGOR: 'gregor'
+};
+
+export const RESISTANCE_LEVEL = ['inefficient', 'normal', 'fatal'];
+export const AttackType = ['slash', 'pierce', 'blunt'];
+export const SinAffinity = ['wrath', 'lust', 'sloth', 'gluttony', 'gloom', 'pride', 'envy'];
+export const Keyword = [
+  'burn',
+  'bleed',
+  'tremor',
+  'rupture',
+  'sinking',
+  'poise',
+  'charge',
+  'haste',
+  'attack_power_up'
+];
